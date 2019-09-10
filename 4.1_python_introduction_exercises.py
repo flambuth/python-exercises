0e5096eeb14c9b427bce25590903407b2642de4b
Fredrick Lambuth <flambuth@gmail.com>
2019-09-10 11:30:48 -0500

    will this work

---
+++ 4.1_python_introduction_exercises.py
@@ -0,0 +1,8 @@
+#!/usr/local/anaconda3/bin/python
+#Will running this telefrag the one I did in the cmd line?
+greeting = 'Hellow world form a var in a script'
+
+print(greeting)
+print(1+1)
+
+#VS Code. On a Mac. WOah.
