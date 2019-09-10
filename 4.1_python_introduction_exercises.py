0f11961e29221e645fa9be65c6da78cb071dc704
Fredrick Lambuth <flambuth@gmail.com>
2019-09-10 14:13:42 -0500

    playing with jupyter

--- 4.1_python_introduction_exercises.py
+++ 4.1_python_introduction_exercises.py
@@ -1,6 +1,6 @@
 #!/usr/local/anaconda3/bin/python
 #Will running this telefrag the one I did in the cmd line?
-greeting = 'Hellow world form a var in a script'
+greeting = 'Hellow world form a var in a pyScript'
 
 print(greeting)
 print(1+1)
