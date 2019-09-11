8beb5346f74ea676d46ce8fda1745c881a58201a
Fredrick Lambuth <flambuth@gmail.com>
2019-09-11 15:56:08 -0500

    up to the break and pass section

--- 4.3_control_structures_exercises.py
+++ 4.3_control_structures_exercises.py
@@ -76,4 +76,15 @@ for i in range(1,11):
 #1
 #22
 #333
-q = int(input('Please input a number: '))
+q = input('Please input a number: ')
+count = 1
+for i in range(1,int(q)+1):
+    print(str(count)*count)
+    count += 1
+
+# 4         BREAK and CONTINUE
+#    Prompt the user for an odd number between 1 and 50. Use a loop and a break statement to continue prompting 
+#    the user if they enter invalid input. (Hint: use the isdigit method on strings to determine this). 
+#    Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number 
+#    the user entered.
+
