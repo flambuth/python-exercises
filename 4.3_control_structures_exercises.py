e6456c1f4bd27bd935a75883b84ea6a01cfb985b
Fredrick Lambuth <flambuth@gmail.com>
2019-09-11 14:55:15 -0500

    more conditional exercises

--- 4.3_control_structures_exercises.py
+++ 4.3_control_structures_exercises.py
@@ -11,7 +11,20 @@ else:
 # prompt the user for a day of the week, print out whether the day is a weekday or a weekend
 days_of_the_week = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
 user_day = input('Please enter the day of the week ')
-if user_day.lower() == days_of_the_week[0] or user_day.lower() == days_of_the_week[-1]
+if days_of_the_week.index(user_day.capitalize()) == 0 or days_of_the_week.index(user_day.capitalize()) == 6:
     print('It is the weekend')
 else:
     print('Nest pas the weekend')
+
+# 1C
+# create variables and make up values for
+#   the number of hours worked in one week
+#   the hourly rate
+#   how much the week's paycheck will be
+
+#   write the python code that calculates the weekly paycheck. 
+#  You get paid time and a half if you work more than 40 hours
+hourly_rate = int(input("how much you make an hour? "))
+hours_per_day = int(input("how long is your workday? "))
+hours_worked = days_of_the_week.index(user_day.capitalize()) * hours_per_day
+paycheck = hourly_rate * hours_worked
