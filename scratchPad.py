3d5171cebc51172ff2f8e2858fb1d51ac2503f1e
Fredrick Lambuth <flambuth@gmail.com>
2019-09-12 11:19:50 -0500

    figured out the exit or continue clause in the scratchpad

--- scratchPad.py
+++ scratchPad.py
@@ -1,7 +1,16 @@
-fruits = ['mango', 'kiwi', 'strawberry', 'guava', 'pineapple', 'mandarin orange']
+is = ['mango', 'kiwi', 'strawberry', 'guava', 'pineapple', 'mandarin orange']
 
-x = [fruit for fruit in fruits if
-(fruit.count('a') + fruit.count('e') + fruit.count('i') + fruit.count('o') + fruit.count('u'))> 2
+x = i for i in fruits if i.count('a') +i.count('e') +i.count('i') +i.count('o') +i.count('u')) > 2
 ]
 
 
+######## lets figure out the continue y/n game
+
+good2go == True
+while good2go:
+    print("we are inside the while loop that happens when good2go is true")
+    player_choice = input("Give me some version of no to break out of this loop. If you dont, you'll see these lines again.")
+    if player_choice.upper() == 'N' or player_choice.upper() == "NO":
+        break
+    else:
+        continue
