0abeec2328678c39a6bdcd91136245532c73bcaf
Fredrick Lambuth <flambuth@gmail.com>
2019-09-13 16:52:24 -0500

    setup next Monday's exercises

---
+++ 4.5_import_exercises.py
@@ -0,0 +1,10 @@
+# 1
+# Import and test 3 of the functions from your functions exercise file.
+
+# Your functions exercise are not currently in a file with a name that can be easily imported. Copy your functions exercise file and name the copy functions_exercises.py.
+
+# Import each function in a different way:
+
+# import the module and refer to the function with the . syntax
+# use from to import the function directly
+# use from and give the function a different name
