da3ccd43c68ccb8528ee6e7aa22253af4ba7c4a4
Fredrick Lambuth <flambuth@gmail.com>
2019-09-16 16:57:56 -0500

    got the view balance feature working

--- menu.py
+++ menu.py
@@ -9,23 +9,39 @@ known_users = [
 {'name':'btwo', 'balance':500}
 ]
 
+def check_user(user, user_list):
+    user_names = [i['name'] for i in user_list]
+    return user in user_names
+
+def see_balance(user, user_list):
+    user_balance = [i['balance'] for i in user_list if i['name'] == user]
+    return user_balance
+
 def print_menu():
     print( 30 * "-" , "MENU" , 30 * "-")
-    print( "1. Menu Option 1")
-    print( "2. Menu Option 2")
-    print( "3. Menu Option 3")
-    print( "4. Menu Option 4")
+    print( "1. View Current Balance")
+    print( "2. Make A Deposit")
+    print( "3. Make A Withdrawal")
+    print( "4. Wish there was a fourth option")
     print( "5. Exit")
     print( 67 * "-")
   
 loop=True      
-  
+
+current_user = input("What is your user name? ")
+if check_user(current_user, known_users):
+    print(" welcome back")
+else:
+    print(" A new user!")
+
 while loop:          ## While loop which will keep going until loop = False
     print_menu()    ## Displays menu
     choice = int(input("Enter your choice [1-5]: "))
      
     if choice==1:     
-        print("Menu 1 has been selected")        ## You can add your code or functions here
+        print("Menu 1 has been selected") 
+        current_balance = see_balance(current_user, known_users)
+        print(f"Your current balance is ${str(current_balance)[1:-1]}")
     elif choice==2:
         print("Menu 2 has been selected")
         ## You can add your code or functions here
