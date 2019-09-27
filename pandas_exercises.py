da79ba6a397879183cfa9cbdad63cb84c15ebd6c
Fredrick Lambuth <flambuth@gmail.com>
2019-09-27 11:53:07 -0500

    starting third exercise

--- pandas_exercises.py
+++ pandas_exercises.py
@@ -42,8 +42,75 @@ mpg.groupby('manufacturer').model.count()
 
 ##########
 # 1
-# 
+# How many different models are there?
 
+# I went with hwy as the field to measure because it was only 3 characters of typing.
+mpg.groupby('model').hwy.count()
+
+
+##########
+# 1
+# Do automatic or manual cars have better miles per gallon?
+
+#gonna group up the manual cars over on to the rows section. See there values in the mileage
+#column
+x= mpg.groupby('trans').mileage.agg(['min','mean','max'])
+
+#Apparently there are more than one type of automatic and manual transmissions.
+#Gonna try to glob them into two groups. Auto and Manual
+
+#this is just the automatics, sliced by their explicit index.
+auto_trans = x.loc[:"auto(s6)"]
+#This is the manuals, slicing the other way
+man_trans = x.loc["manual(m5)":]
+
+#the mileage average for all transmission types
+x['mean'].agg('mean')
+#mileage for the autos
+auto_trans['mean'].agg('mean')
+#mileage for the manuals
+man_trans['mean'].agg('mean')
+
+#Lets put out a nice string 
+print(f"Automatics get {auto_trans['mean'].agg('mean')} mpg and manuals get {man_trans['mean'].agg('mean')} mpg.")
+print(f"{abs(auto_trans['mean'].agg('mean') - man_trans['mean'].agg('mean'))} does not seem statistically significant. I'd go with the convenience of the auto.")
+
+
+
+###########################################
+#                       Joining and Merging
+# 2
+# Copy the users and roles dataframes from the examples above. 
+# What do you think a right join would look like? 
+# An outer join? 
+# What happens if you drop the foreign keys from the dataframes and try to merge them?
+
+
+###########################################
+#                       Getting data from SQL data 
+# 3
+# Create a function named get_db_url. It should accept a username, hostname, password, and 
+# database name and return a url formatted like in the examples in this lesson.
+
+# Use your function to obtain a connection to the employees database.
+# Once you have successfully run a query:
+#   Intentionally make a typo in the database url. What kind of error message do you see?
+
+#   Intentionally make an error in your SQL query. What does the error message look like?
+
+# Read the employees and titles tables into two separate dataframes
+
+# Visualize the number of employees with each title.
+
+# Join the employees and titles dataframes together.
+
+# Visualize how frequently employees change titles.
+
+# For each title, find the hire date of the employee that was hired most recently with that 
+# title.
+
+# Write the code necessary to create a cross tabulation of the number of titles by department. 
+# (Hint: this will involve a combination of SQL and python/pandas code)
 
 
 #Don't forget to change your db. 
