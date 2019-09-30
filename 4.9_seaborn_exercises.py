b80671fd14f8d73d89658cef3f83461f9e5a384a
Fredrick Lambuth <flambuth@gmail.com>
2019-09-30 11:03:30 -0500

    boxes are for distros

---
+++ 4.9_seaborn_exercises.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import seaborn as sns
+
+#what is pylint? did'nt I alraedy have that?
+#I messwed with that 'base':conda when I was working with the flask stuff over the weekend
+
+
+# Use the iris database to answer the following quesitons:
+
+iris = sns.load_dataset('iris')
+
+
+# What does the distribution of petal lengths look like?
+
+
+
+# Is there a correlation between petal length and petal width?
+# Would it be reasonable to predict species based on sepal width and sepal length?
+# Which features would be best used to predict species?
